97ea685705e62f0eafd21b0049c54a6865d0c4e9
João Valente <filipevalente@gmail.com>
2013-10-22 11:56:10 +0100

    user model: serialises personal_info and expert_info as hash; adds role awareness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+  ROLES = %w[admin expert]
+  # default_scope { order('created_at DESC') }
+  serialize :personal_info, Hash
+  serialize :expert_info, Hash
+
+  # ROLE MANAGER LOGIC
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
+
+  def is?(role)
+    roles.include?(role.to_s)
+  end
+  # (end) ROLE MANAGER LOGIC
 end
