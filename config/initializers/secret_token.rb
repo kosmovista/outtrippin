13e3f05c222df826f1e41100add2b7edf81e3dd1
João Valente <filipevalente@gmail.com>
2013-10-22 11:29:59 +0100

    rails 4 app with rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Outtrippin::Application.config.secret_key_base = '631cd591c9007b5403aec34e71e626bf4d697630c1f1f5169b1174f8946088be1ef49d5d2a3939131f4a82fd087b35aa722c540b3fc8b5034640073ac713f909'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Outtrippin::Application.config.session_store :cookie_store, key: '_outtrippin_session'
