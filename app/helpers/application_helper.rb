c7d5f514514b06b2aeb0fce76af3a3b52027fc3f
João Valente <filipevalente@gmail.com>
2013-10-28 21:52:19 +0000

    refactors errors_form on forms.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def errors_for(object, field)
+    "<small class='error hide'>#{parse_error_message object.errors.messages[field]}</small>".html_safe
+  end
+
+  private
+
+  def parse_error_message(messages)
+    messages_string = ""
+    messages.each { |m| messages_string << m.capitalize + "<br/>" }
+    messages_string
+  end
 end
