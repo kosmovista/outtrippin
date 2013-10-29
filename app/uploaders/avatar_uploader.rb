9bf1019e69faac997bdcf7a303df753fdc1c2856
João Valente <filipevalente@gmail.com>
2013-10-29 19:05:46 +0000

    bugfix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,9 +3,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-  # storage :fog
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
