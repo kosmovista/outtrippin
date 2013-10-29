0a68288dbf7f7b1e1aefe3279f36419e87b40403
João Valente <filipevalente@gmail.com>
2013-10-29 00:04:44 +0000

    adds admin dashboard.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authorize
+
   def index
 
+  end
+
+  private
+
+  def authorize
+    redirect_to root_path unless current_user && current_user.is?("admin")
   end
 end
