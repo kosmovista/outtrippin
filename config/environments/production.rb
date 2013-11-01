12a1be34a76ee83e6d92041c649db2cba259e6e8
João Valente <filipevalente@gmail.com>
2013-11-01 18:22:36 +0000

    adds homepage.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Outtrippin::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( experts_registration.css experts.css )
+  config.assets.precompile += %w( experts_registration.css experts.css home.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
