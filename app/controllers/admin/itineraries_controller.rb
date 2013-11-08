eb58155b02aa62828943f8c46ee9a20defbdc61f
João Valente <filipevalente@gmail.com>
2013-11-08 18:56:45 +0000

    allows admin to destroy itineraries.

--- app/controllers/admin/itineraries_controller.rb
+++ app/controllers/admin/itineraries_controller.rb
@@ -2,6 +2,11 @@ class Admin::ItinerariesController < ApplicationController
   before_action :authorize_admin
 
   def index
+  end
 
+  def destroy
+    @itinerary = Itinerary.find(params[:id])
+    @itinerary.delete
+    redirect_to :back
   end
 end
