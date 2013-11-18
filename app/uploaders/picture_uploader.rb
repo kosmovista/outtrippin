e4cc8fcec53e7c784fcdf0ed5091a7e35def5a24
João Valente <filipevalente@gmail.com>
2013-11-18 14:31:12 +0000

    adds picture.rb and picture uploader.

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
