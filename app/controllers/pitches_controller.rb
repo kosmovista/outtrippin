63b338c9db14ab708a6026b6f62a88980fb60283
João Valente <filipevalente@gmail.com>
2013-11-18 15:50:50 +0000

    expert can now edit a pitch.

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -3,6 +3,7 @@ class PitchesController < ApplicationController
 
   # should guarantee an EXPERT!
   before_action :set_itinerary
+  before_action :set_pitch, only: [:edit, :update]
 
   def new
     @pitch_new = PitchNew.new(itinerary: @itinerary, user: current_user)
@@ -18,11 +19,24 @@ class PitchesController < ApplicationController
   end
 
   def edit
-    @pitch_new = PitchNew.new(itinerary: @itinerary, user: current_user)
+    @pitch_edit = PitchEdit.new(itinerary: @itinerary, user: current_user, pitch: @pitch)
+  end
+
+  def update
+    @pitch_edit = PitchEdit.new(itinerary: @itinerary, user: current_user, pitch: @pitch, attributes: params[:pitch_edit])
+    if @pitch_edit.save
+      redirect_to itinerary_path(@itinerary)
+    else
+      render 'edit'
+    end
   end
 
   def set_itinerary
     @itinerary = Itinerary.find(params[:itinerary_id])
+  end
+
+  def set_pitch
+    @pitch = Pitch.find(params[:id])
   end
 
 end
--- app/forms/pitch_edit.rb
+++ app/forms/pitch_edit.rb
@@ -1,4 +1,4 @@
-class PitchNew
+class PitchEdit
   include ActiveModel::Model
 
   def persisted?
@@ -11,59 +11,61 @@ class PitchNew
 
   def initialize(options = {})
     attributes = options[:attributes]
+    @pitch = options[:pitch]
     @itinerary = options[:itinerary]
     @user = options[:user]
 
     raise ArgumentError, "You need to be an expert" if !@user.is?("expert")
     raise ArgumentError, "You need to supply an itinerary" if @itinerary.nil?
+    raise ArgumentError, "You need to supply a pitch" if @pitch.nil?
 
     unless attributes.nil?
-      send("title=", attributes[:title])
-      send("summary=", attributes[:summary])
-      send("why_me=", attributes[:why_me])
-      send("pictures=", attributes[:pictures])
+      ATTRIBUTES.each do |attribute|
+        send("title=", attributes[:title])
+        send("summary=", attributes[:summary])
+        send("why_me=", attributes[:why_me])
+        send("pictures=", attributes[:pictures])
+      end
+    else
+      send("title=", @pitch.title)
+      send("summary=", @pitch.summary)
+      send("why_me=", @pitch.why_me)
+      send("pictures=", nil)
     end
   end
 
   validate do
     errors[:title] = "Can't be blank" if title.blank?
     errors[:summary] = "Can't be blank" if summary.blank?
-    errors[:why_me] = "Can't be blank" if why_me.blank?
-    errors[:pictures] = "Can't be blank" if pictures.blank?
+    errors[:why_me] = "Can't be blank" if why_me.empty?
+    errors[:pictures] = "Can't be blank" if pictures.empty?
   end
   #
-  def pitch
-    @pitch ||= Pitch.new(title: title, summary: summary, why_me: why_me)
-  end
-  #
+
   def save
     return false unless valid?
-    if create_objects
+    if update_objects
       true
     else
       false
     end
   end
 
-
   private
 
-  def create_objects
-    ActiveRecord::Base.transaction do
+  def update_objects
+    # ActiveRecord::Base.transaction do
       pictures.each do |k, v|
         if v.class == ActionDispatch::Http::UploadedFile
           p = @user.pictures.create(source: v)
-          pitch.pictures << p
+          @pitch.pictures << p
+          @pitch.save
         end
       end
 
-      pitch.user = @user
-      pitch.save!
-      @itinerary.pitches << pitch
-      @itinerary.save!
-    end
-  rescue
-    false
+      @pitch.update_attributes(title: title, summary: summary, why_me: why_me)
+  #   end
+  # rescue
+  #   false
   end
-
 end
