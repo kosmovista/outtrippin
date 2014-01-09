2f28cc02276fc879e31988132c0c27771573380e
João Valente <filipevalente@gmail.com>
2014-01-09 19:19:57 +0000

    gracefully terminates if file does not exist on racksapce.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -30,9 +30,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+
+  def remove!
+    begin
+      super
+    rescue Fog::Storage::Rackspace::NotFound
+    end
+  end
 end
