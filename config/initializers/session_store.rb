41a31bbb0718e50dfc69653612efe55c8cbd5ec1
João Valente <filipevalente@gmail.com>
2014-01-15 15:28:31 +0000

    http:// and www will share session.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Outtrippin::Application.config.session_store :cookie_store, key: '_outtrippin_session', domain: '.outtrippin.com'
+Outtrippin::Application.config.session_store :cookie_store, key: '_outtrippin_session', domain: '.outtrippin.com' if Rails.env.production?
+Outtrippin::Application.config.session_store :cookie_store, key: '_outtrippin_session' if Rails.env.development?
