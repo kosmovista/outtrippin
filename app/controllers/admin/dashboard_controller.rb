36d2149586210c9f76eb04e2f8e92144dea9b078
João Valente <filipevalente@gmail.com>
2014-01-20 20:39:12 +0000

    filtering capabilities on admin backend.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,15 @@ class Admin::DashboardController < ApplicationController
   before_action :authorize_admin
 
   def index
-
+    if params[:filter] == "experts"
+      @users = User.experts.page(params[:page] || 0)
+    elsif params[:filter] == "customers"
+      @users = User.customers.page(params[:page] || 0)
+    elsif params[:filter] == "admins"
+      @users = User.admins.page(params[:page] || 0)
+    else
+      @users = User.page(params[:page] || 0)
+    end
   end
 
   def expert_list
