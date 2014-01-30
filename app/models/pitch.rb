ae287e1b5f49ea585bcf3dcb676ec5d4fd8200d2
João Valente <filipevalente@gmail.com>
2014-01-30 12:08:24 +0000

    bugfix.

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -11,10 +11,12 @@ class Pitch < ActiveRecord::Base
   end
 
   def self.find_by_user(user)
-    Pitch.where(user_id: user.id)
+    pitches = Pitch.where(user_id: user.id)
+    pitches
   end
 
   def self.find_winner_expert_by_itinerary(user)
-    Pitch.where(user_id: user.id, winner: true).map { |p| p.itinerary }
+    pitches = Pitch.where(user_id: user.id, winner: true).map { |p| p.itinerary }
+    pitches
   end
 end
