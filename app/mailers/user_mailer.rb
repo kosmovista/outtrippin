935d116885cc6b2cc1111a873429069da29a4621
Kunal Kalro <kunal@kunal-ubuntu.(none)>
2014-03-03 17:28:05 +1100

    copy changes OutTrippin to Zuji

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,42 +1,42 @@
 class UserMailer < ActionMailer::Base
   helper ActionView::Helpers::UrlHelper
-  default from: "\"OutTrippin\" <contact@outtrippin.com>"
+  default from: "\"Zuji\" <planmytrip@Zuji.com>"
 
   def welcome_expert_email(user)
     @user = user
-    mail to: @user.email, subject: "Welcome to OutTrippin!"
+    mail to: @user.email, subject: "Welcome to Zuji!"
   end
 
   def welcome_user_email(user, password)
     @user = user
     @password = password
-    mail to: @user.email, subject: "Welcome to OutTrippin!"
+    mail to: @user.email, subject: "Welcome to Zuji!"
   end
 
   def payment_received_email(user, itinerary)
     @user = user
     @itinerary = itinerary
-    mail to: @user.email, subject: "Thanks for submitting your OutTrippin trip and payment."
+    mail to: @user.email, subject: "Thanks for submitting your Zuji trip and payment."
   end
 
   def password_reset_instructions_email(user)
     @user = user
-    @reset_url = "http://outtrippin.com/password_resets/#{@user.perishable_token}/edit"
-    mail to: @user.email, subject: "OutTrippin Password Reset Instructions"
+    @reset_url = "http://planmytrip.zuji.com.au/password_resets/#{@user.perishable_token}/edit"
+    mail to: @user.email, subject: "Zuji Password Reset Instructions"
   end
 
   def new_pitch_email(pitch)
     @pitch = pitch
     @itinerary = @pitch.itinerary
     @user = @itinerary.user
-    mail to: @user.email, subject: "New OutTrippin Pitch Received!"
+    mail to: @user.email, subject: "New Zuji Pitch Received!"
   end
 
   def new_pitch_email(pitch)
     @pitch = pitch
     @itinerary = @pitch.itinerary
     @user = @itinerary.user
-    mail to: @user.email, subject: "New OutTrippin Pitch Received!"
+    mail to: @user.email, subject: "New Zuji Pitch Received!"
   end
 
   def new_pitch_expert_email(pitch)
