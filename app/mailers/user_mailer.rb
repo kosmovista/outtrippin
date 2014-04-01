176b5dc6f523d764e9a70353e9f1a3aa1c78ade4
João Valente <filipevalente@gmail.com>
2014-04-01 19:58:16 +0100

    removes zuji smtp settings.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -21,7 +21,7 @@ class UserMailer < ActionMailer::Base
     @password = password
     @itinerary = itinerary
     if @itinerary.extra_info.has_key?(:source)
-      mail to: @user.email, subject: "Welcome to Zuji!", from: "\"Zuji\" <planmytrip@zuji.com.au>", delivery_method_options: zuji_smtp_settings
+      mail to: @user.email, subject: "Welcome to Zuji!", from: "\"Zuji\" <planmytrip@zuji.com.au>"
     else
       mail to: @user.email, subject: "Welcome to OutTrippin!"
     end
@@ -41,7 +41,7 @@ class UserMailer < ActionMailer::Base
     @user = user
     @itinerary = itinerary
     if @itinerary.extra_info.has_key?(:source)
-      mail to: @user.email, subject: "Thanks for submitting your Zuji trip and payment.", from: "\"Zuji\" <planmytrip@zuji.com.au>", delivery_method_options: zuji_smtp_settings
+      mail to: @user.email, subject: "Thanks for submitting your Zuji trip and payment.", from: "\"Zuji\" <planmytrip@zuji.com.au>"
     else
       mail to: @user.email, subject: "Thanks for submitting your OutTrippin trip and payment."
     end
@@ -92,7 +92,7 @@ class UserMailer < ActionMailer::Base
     @user = @itinerary.user
 
     if @itinerary.extra_info.has_key?(:source)
-      mail to: @user.email, subject: "New Zuji Pitch Received!", from: "\"Zuji\" <planmytrip@zuji.com.au>", delivery_method_options: zuji_smtp_settings
+      mail to: @user.email, subject: "New Zuji Pitch Received!", from: "\"Zuji\" <planmytrip@zuji.com.au>"
     else
       mail to: @user.email, subject: "New OutTrippin Pitch Received!"
     end
