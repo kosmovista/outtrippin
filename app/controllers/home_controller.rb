ff00dbaae57de8387c8eca306e1f7ea07bf64802
João Valente <filipevalente@gmail.com>
2014-04-10 16:03:18 +0100

    won't call set_cities on home_controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   before_action :set_homepage_type
   before_action :set_white_topbar
-  before_action :set_cities
 
   def index
     @itinerary = Itinerary.new
