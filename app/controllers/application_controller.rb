a608f71eef855cccf0f35ebaa2bccea1a43b2a25
João Valente <filipevalente@gmail.com>
2014-04-28 18:19:29 +0100

    adds experimental browse page using plans api.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     @current_user_session = UserSession.find
   end
 
+  def browsexperimental
+    render layout: false
+  end
+
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.record
