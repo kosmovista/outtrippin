a75484f895d928cad8752dbdb0a4af174a9d375c
João Valente <filipevalente@gmail.com>
2014-05-25 01:22:45 +0100

    browse links bugfix

--- app/assets/javascripts/new_application.js
+++ app/assets/javascripts/new_application.js
@@ -15,6 +15,6 @@ $.get("/api/v1/plans")
     var container = $("#plans-container");
     $.each(data, function(i, item) {
       // append the newly loaded plan to the list
-      container.append('<li><a class="go-to-itinerary" href="/itineraries/' + item.id + ' "><p class="title">' + item.title + '</p><img src="' + item.cover + '" onload="imgLoaded(this)"><a class="author" href="/users/' + item.author.id + '"><img src="' + item.author.avatar + '" onload="imgLoaded(this)"><p class="author-name">Recommended by '+ item.author.name + '</p></a></a></li>');
+      container.append('<li><a class="go-to-itinerary" href="/itineraries/' + item.id + '/plan "><p class="title">' + item.title + '</p><img src="' + item.cover + '" onload="imgLoaded(this)"><a class="author" href="/users/' + item.author.id + '"><img src="' + item.author.avatar + '" onload="imgLoaded(this)"><p class="author-name">Recommended by '+ item.author.name + '</p></a></a></li>');
     });
   });
