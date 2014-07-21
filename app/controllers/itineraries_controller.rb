a4bece448e43da452ba3cced53537d53ffeb4b1d
João Valente <filipevalente@gmail.com>
2014-07-21 22:20:43 +0100

    will get automatic pitches even when the user has to go login.

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -52,6 +52,9 @@ class ItinerariesController < ApplicationController
     @itinerary_finalize = ItineraryFinalize.new({attributes: params[:itinerary_finalize], itinerary: @itinerary})
     place = Place.find_by_name(@itinerary.destination.downcase.strip)
     if @itinerary_finalize.save
+          # will copy the pitches that exist for this place
+          # into the current itinerary (and reset the winners)
+      @itinerary.get_pitches_from_place(place)
       if !current_user
         flash[:notice] = "Looks like there's already an account with that email address. Please login to continue! :)"
         session[:original_uri] = checkout_itinerary_path(@itinerary)
@@ -61,9 +64,6 @@ class ItinerariesController < ApplicationController
         if place.nil?
           redirect_to checkout_itinerary_path(@itinerary)
         else
-          # will copy the pitches that exist for this place
-          # into the current itinerary (and reset the winners)
-          @itinerary.get_pitches_from_place(place)
           redirect_to itinerary_path(@itinerary)
         end
       end
