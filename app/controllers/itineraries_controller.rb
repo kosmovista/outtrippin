a9af27369d1125014964b91b29cc284bb01216d7
Kunal Kalro <kunal@kunal-ubuntu.(none)>
2014-07-25 16:14:28 +1000

    bugfix on payment

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -99,7 +99,6 @@ class ItinerariesController < ApplicationController
 
   def purchase
     token = params[:stripeToken]
-    plan = params[:plan].to_i
 
     if @itinerary.process_payment(token, plan)
       UserMailer.delay.payment_received_email(@itinerary.user, @itinerary)
